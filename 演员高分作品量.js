3665d076fc6d8c8d8d36b18658b6e792e9c4dc4b
nice <244039784@qq.com>
2018-11-04 11:23:19 +0800

    统计图

---
+++ 演员高分作品量.js
@@ -0,0 +1,15 @@
+var myChart = echarts.init(document.getElementById('main'));
+var option = {
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  },
+  yAxis: {
+    type: 'value'
+  },
+  series: [{
+    data: [120, 200, 150, 80, 70, 110, 130],
+    type: 'bar'
+  }]
+};
+myChart.setOption(option);
